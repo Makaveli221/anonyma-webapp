33859b13879ef0747da2597aff958b42c9a50ec0
Ousmane Ndiaye <ousmanendiaye352@gmail.com>
2020-04-04 09:12:22 +0000

    add sujet and thematique in page single topic

--- src/app/modules/topic/pages/index/index.component.ts
+++ src/app/modules/topic/pages/index/index.component.ts
@@ -12,16 +12,33 @@ export class IndexComponent implements OnInit, AfterViewInit {
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
   ngAfterViewInit(): void {
-    var elems = document.querySelectorAll('.fixed-action-btn');
-    var instances = M.FloatingActionButton.init(elems, {});
+    M.FloatingActionButton.init(document.querySelectorAll('.fixed-action-btn'), {});
   }
 
   scrollToElement($element): void {
     console.log($element);
     $element.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+  }
+
+  animateButton(id) {
+    let btn = document.querySelector('#'+ id) as HTMLButtonElement;
+    //reset animation
+    btn.classList.remove('animate');
+    
+    btn.classList.add('animate');
+    setTimeout(function(){
+      btn.classList.remove('animate');
+    },700);
+  }
+
+  updateListThematique(event, sujet: string) {
+    document.querySelector('#sujet-forum a.active').classList.remove('active');
+    (event.target as HTMLElement).classList.add('active');
+    this.scrollToElement(document.querySelector('#thematiques-sujet'));
   }
 
 }
